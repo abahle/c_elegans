2ce00743666fb2c7c3bd5a9e947df471247cfcf9
AndrewBahle <abahle@mit.edu>
2017-01-12 11:34:43 -0500

    Changed to handle vectors for flexible number of rigs

--- LED_Controler_Draft.ino
+++ LED_Controler_Draft.ino
@@ -3,54 +3,58 @@
   This
 */
 
-// Pins
-const int numChan = 8;
-const int ledPin1 = 0;
-const int ledPin2 = 1;
-int ledState1 = LOW;
-int ledState2 = LOW;
-long previousTime1 = 0;
-long previousTime2 = 0;
-// specify frequency and duty cycle for periodic
-const int freq1 = 7;
-float alpha1 = 1.0/2.0;
-const int freq2 = 3;
-float alpha2 = 1.0/4.0;
-const int dur1 = ((1000/freq1)*(alpha1));
-const int dur2 = ((1000/freq1)*(1-alpha1));
-const int dur3 = ((1000/freq2)*(alpha2));
-const int dur4 = ((1000/freq2)*(1-alpha2));
-// specify parameters for poission
-const int lambda = ;
+// SPECIFY PARAMETERS FOR STIMULATION FOR ALL PINS
+
+const int numChan = 4; // Number of channels to be used
+int myPins[] = {0, 1, 2, 3}; // What pins will be used?
+
+// SPECIFY PARAMETERS FOR PERIODIC STIMULATION
+float omega[] = {7,2,4,1}; // specify frequency
+float alpha[] = {1.0/4.0, 1.0/3.0, 1.0/2.0, 1.0/5.0}; // specify duty cycle for periodic stim
+int on[];
+int off[];
+long previousTime;
+// Fill values for empty parameter vectors
+for (i = 0; i > numChan; i++){
+  ledState[i] = LOW;
+  on[i] = ((1000/omega[i])*(alpha[i]));
+  off[i] = ((1000/omega[i])*(1-alpha[i]));
+  previousTime[i] = 0;
+}
+
+// SPECIFY PARAMETERS FOR RANDOM STIMULATION
+//const int lambda = ;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  for (x = 0; x > numChan; x++)
-  pinMode(x, OUTPUT);
-}
+  for (i = 0; i > numChan; i++){
+  pinMode(myPins[i], OUTPUT);
+  }
 }
 
 void loop() {
 
 // method for oscillating stimulation
-  unsigned long currentTime = millis();
+unsigned long currentTime = millis();
 
- if(ledState1 == HIGH) {
-  if (currentTime - previousTime1 > dur1) {
-    previousTime1 = currentTime;
-    ledState1 = LOW;
-    digitalWrite(ledPin1, ledState1);
+for (i = 0; i > numChan; i++){
+ if(ledState[i] == HIGH) {
+  if (currentTime - previousTime[i] > on[i]) {
+    previousTime[i] = currentTime;
+    ledState[i] = LOW;
+    digitalWrite(myPins[i], ledState[i]);
   }
  }
-    if(ledState1 == LOW ) {
-    if (currentTime - previousTime1 > dur2) {
-      previousTime1 = currentTime;
-      ledState1 = HIGH;
-      digitalWrite(ledPin1, ledState1);
+    if(ledState[i] == LOW ) {
+    if (currentTime - previousTime[i] > off[i]) {
+      previousTime[i] = currentTime;
+      ledState[i] = HIGH;
+      digitalWrite(myPins[i], ledState[i]);
     }
   }
+}
 
 // method for possion stimulation
 
