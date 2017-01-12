e308b8c83c0f1b5eb1696b5bce22ba3e2d94cd61
AndrewBahle <abahle@mit.edu>
2017-01-11 19:14:29 -0500

    Started updating to use mills instead of delay so that we can control multiple rigs in parallel

--- LED_Controler/LED_Controler.ino
+++ LED_Controler/LED_Controler.ino
@@ -4,32 +4,34 @@
 */
 
 // Pins
-const int LED_PIN1 = 0;
-const int freq = 1;
-float duty = 1.0/2.0;
+const int ledPin = 0;
+int ledState = LOW;
+long previousTime = 0;
 // frequency and duty cycle for periodic and poisson stimulation
+const int freq = 1;
+//float duty = 1.0/2.0;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN1, OUTPUT); 
-  //Serial.begin(9600);
-  //while (! Serial); // Wait until Serial is ready - Leonardo
-  //Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
+  pinMode(ledPin, OUTPUT); 
 }
 
-// the loop function runs over and over again forever
-void loop() {
-       for(int x = 0; x <= freq-1; x++){
-      digitalWrite(LED_PIN1, HIGH);   // turn the LED on (HIGH is the voltage level)
-      //delay((1000/omega)*alpha);                       // wait for a second
-      delay((1000/freq)*duty);
-      digitalWrite(LED_PIN1, LOW);    // turn the LED off by making the voltage LOW
-      //delay((1000/omega)*(1-alpha)); 
-      delay((1000/freq)*(1-duty));
-       }
-       //how to operate two simulataneously
-       
+void loop()
+{
+  unsigned long currentTime = millis();
+  
+  if(currentTime - previousTime > (1000/freq) {
+    previousTime = currentTime;
+    
+    if(ledStat == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+    
+    digitalWrite(ledPin, ledState);
+  }
 }
 
 
